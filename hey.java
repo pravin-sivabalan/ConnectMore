f6bc1bd57fe8b6b860f1746544f36a5ddc9c5d39
Hiten98 <hitenrathod98@gmail.com>
2016-10-15 21:58:11 -0400

    Add files via upload

---
+++ hey.java
@@ -0,0 +1,10 @@
+
+public class hey {
+public static void main(String[] args){
+	for(int i = 0; i < 7; i++)
+		for(int j = 0; j < 7; j++){
+			System.out.println("obj"+i+j+".text = a.board["+i+"]["+j+"]");
+		}
+	}
+}
+
